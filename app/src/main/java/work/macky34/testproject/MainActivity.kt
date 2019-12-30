113b714d2623696cb5321e705d7d47328a718ac6
Takuya <yg.ssb63@gmail.com>
2019-12-31 04:02:27 +0900

    initialization

---
+++ app/src/main/java/work/macky34/testproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package work.macky34.testproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "work.macky34.testproject.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Called when the user taps the Send button
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
